bc7a708678e9f6ee91f805665a558e03e1d20656
steven.vos <steven.vos@iodigital.com>
2024-08-28 08:50:52 +0200

    feat: challenge due

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import { reactive } from "vue";
 import { movies } from "./helpers/movies.ts";
 
 const moviesList = reactive(movies);
+
+const setRating = (movie: any, rating: number) => {
+  movie.rating = rating;
+};
 </script>
 
 <template>
@@ -36,23 +40,25 @@ const moviesList = reactive(movies);
             <div class="flex">
               Rating: ({{ movie.rating }}/5)
               <ul class="flex items-center gap-1 ml-2">
-                <li
-                  v-for="(star, ind) in movie.rating"
-                  :key="`rating-${ind}`"
-                  class="text-yellow-500"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    class="size-6"
+                <li v-for="(star, ind) in 5" :key="`rating-${ind}`">
+                  <button
+                    :class="ind < movie.rating ? 'text-yellow-500' : 'text-gray-500'"
+                    :disabled="movie.rating === ind + 1"
+                    @click="setRating(movie, ind + 1)"
                   >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="size-6"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </button>
                 </li>
               </ul>
             </div>
