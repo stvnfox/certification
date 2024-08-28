9a68e38849cab9efb8d5b3acf7312c4f0071a28b
steven.vos <steven.vos@iodigital.com>
2024-08-28 09:49:10 +0200

    feat: challenge tre

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,30 @@ const setRating = (movie: any, rating: number) => {
         <li
           v-for="movie in moviesList"
           :key="movie.id"
-          class="flex flex-col bg-white rounded-md"
+          class="relative flex flex-col bg-white rounded-md"
         >
           <img
             :src="movie.image"
             alt=""
             class="rounded-t-md h-[400px] object-cover w-full"
           />
+          <div class="absolute right-2 top-2 text-yellow-500 h-12 w-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-12"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <span class="text-gray-500 font-bold absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+              {{ movie.rating ?? '-' }}
+            </span>
+          </div>
           <div class="flex flex-col flex-grow p-4">
             <h2 class="text-xl font-semibold">{{ movie.name }}</h2>
             <ul class="flex items-center gap-1 mt-1 mb-2">
